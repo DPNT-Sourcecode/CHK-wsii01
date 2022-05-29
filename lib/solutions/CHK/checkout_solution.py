630fa08e2e88c8106e09956f1f98787f3e9f66b0
sbx_user1051 <sbx_user1051@169.254.85.93>
2022-05-29 16:46:30 +0000

    Sun May 29 16:46:30 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,35 @@
-
+import re
+from collections import defaultdict
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    if not bool(re.match('^[A-D]+$', skus)):
+        return -1
+    
+    sku_dict = defaultdict(int)
+    for item in skus:
+        if item == 'A':
+            sku_dict[item] += 50
+            eligible = sku_dict[item]/150
+            if eligible >= 1:
+                sku_dict[item] -= eligible * 20
+        if item == 'B':
+            sku_dic[item] += 30
+            eligible = sku_dic[item]/60
+            if eligible >= 1:
+                sku_dict[item] -= eligible *15
+        if item == 'C':
+            sku_dict[item] += 20
+        if item == 'D':
+            sku_dict[item] += 15
+
+    sum = 0
+    for value in sku_dict.values():
+        sum += value
+
+    return sum
+        
+
+    
+
