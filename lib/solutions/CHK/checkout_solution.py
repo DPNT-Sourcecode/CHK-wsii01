0f4d1afb98fa1d00cfa31afe89366b73a82da7d8
sbx_user1051 <sbx_user1051@169.254.85.93>
2022-05-29 17:01:33 +0000

    Sun May 29 17:01:33 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,19 +6,19 @@ from collections import defaultdict
 def checkout(skus):
     if not bool(re.match('^[A-D]+$', skus)):
         return -1
-    
+
     sku_dict = defaultdict(int)
     for item in skus:
         if item == 'A':
             sku_dict[item] += 50
             eligible = sku_dict[item]//150
             if eligible >= 1:
-                sku_dict[item] -= eligible * 20
+                sku_dict[item] -= 20
         if item == 'B':
             sku_dict[item] += 30
             eligible = sku_dict[item]//60
             if eligible >= 1:
-                sku_dict[item] -= eligible *15
+                sku_dict[item] -= 15
         if item == 'C':
             sku_dict[item] += 20
         if item == 'D':
@@ -32,4 +32,5 @@ def checkout(skus):
         
 
     
+
 
