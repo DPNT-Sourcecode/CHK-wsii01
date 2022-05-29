0c09dcf63162f269428b19456c56a4dcb0de35d0
sbx_user1051 <sbx_user1051@169.254.85.93>
2022-05-29 17:12:21 +0000

    Sun May 29 17:12:21 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,21 +7,32 @@ def checkout(skus):
     if not bool(re.match('^[A-D]+$', skus)):
         return -1
 
+    price_A = 50
+    price_B = 30
+    price_C = 20
+    price_D = 15
+
+    disc_A = 20
+    disc_B = 15
+
+    eligible_A = 150
+    eligible_B = 60
+    
     sku_dict = defaultdict(int)
     for item in skus:
         if item == 'A':
-            sku_dict[item] += 50
+            sku_dict[item] += price_A
         if item == 'B':
-            sku_dict[item] += 30
+            sku_dict[item] += price_B
         if item == 'C':
-            sku_dict[item] += 20
+            sku_dict[item] += price_C
         if item == 'D':
-            sku_dict[item] += 15
+            sku_dict[item] += price_D
 
-    disc_fac = sku_dict['A'] // 150
-    sku_dict['A'] -= disc_fac * 20
-    disc_fac = sku_dict['B'] // 60
-    sku_dict['B'] -= disc_fac * 15
+    disc_fac = sku_dict['A'] // eligible_A
+    sku_dict['A'] -= disc_fac * disc_A
+    disc_fac = sku_dict['B'] // eligible_B
+    sku_dict['B'] -= disc_fac * disc_B
     
     sum = 0
     for value in sku_dict.values():
@@ -31,6 +42,7 @@ def checkout(skus):
         
 
     
+
 
 
 
